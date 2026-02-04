a793531fccd461247aa4608d2f5e037a34d1c966
paolo-della-fera <paolodf2005@gmail.com>
2026-02-04 16:15:43 +0100

    complete the fourth task of the bonus exercise

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -13,7 +13,6 @@ const teachers = [
 const reversedTeachers = teachers.reverse();
 console.log(reversedTeachers);
 
-
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 const longNames = [];
@@ -26,7 +25,6 @@ for (let i = 0; i < teachers.length; i++) {
 }
 console.log(longNames);
 
-
 // 3. Rimuovi 'Ed' dall'array teachers
 const removeTeachers = teachers.indexOf('Ed');
 
@@ -36,10 +34,9 @@ if (removeTeachers >= 0) {
 
 console.log(teachers);
 
-
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.indexOf('Fabio') >= 0;
+console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
